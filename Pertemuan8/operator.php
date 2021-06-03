80e2ef59e49183a404377bda586431af2ae772f3
Irsyad Purbha <irsyadpurbha@gmail.com>
2021-06-03 20:12:42 +0700

    UPDATE 20210603 20:12

--- Pertemuan8/operator.php
+++ Pertemuan8/operator.php
@@ -1,11 +1,10 @@
 <?php
-	if (isset($_POST['operator']))
-	{
-		//Barang
-		if ($_POST['operator'] == "add_barang") { input_barang(); }
-		if ($_POST['operator'] == "edit_barang") { edit_barang(); }
-		if ($_POST['operator'] == "delete_barang") { delete_barang(); }
-		//Pelanggan
-		if ($_POST['operator'] == "add_pelanggan") { input_pelanggan(); }
-	}
+if (isset($_POST['operator'])) {
+	//Barang
+	if ($_POST['operator'] == "add_barang") { input_barang(); }
+	if ($_POST['operator'] == "edit_barang") { edit_barang(); }
+	if ($_POST['operator'] == "delete_barang") { delete_barang(); }
+	//Pelanggan
+	if ($_POST['operator'] == "add_pelanggan") { input_pelanggan(); }
+}
 ?>
--- Pertemuan9/operator.php
+++ Pertemuan9/operator.php
@@ -1,13 +1,12 @@
 <?php
-	if (isset($_POST['operator']))
-	{
-		//Barang
-		if ($_POST['operator'] == "add_barang") { input_barang(); }
-		if ($_POST['operator'] == "edit_barang") { edit_barang(); }
-		if ($_POST['operator'] == "delete_barang") { delete_barang(); }
-		//Pelanggan
-		if ($_POST['operator'] == "add_pelanggan") { input_pelanggan(); }
-		if ($_POST['operator'] == "edit_pelanggan") { edit_pelanggan(); }
-		if ($_POST['operator'] == "delete_pelanggan") { delete_pelanggan(); }
-	}
+if (isset($_POST['operator'])) {
+	//Barang
+	if ($_POST['operator'] == "add_barang") { input_barang(); }
+	if ($_POST['operator'] == "edit_barang") { edit_barang(); }
+	if ($_POST['operator'] == "delete_barang") { delete_barang(); }
+	//Pelanggan
+	if ($_POST['operator'] == "add_pelanggan") { input_pelanggan(); }
+	if ($_POST['operator'] == "edit_pelanggan") { edit_pelanggan(); }
+	if ($_POST['operator'] == "delete_pelanggan") { delete_pelanggan(); }
+}
 ?>
